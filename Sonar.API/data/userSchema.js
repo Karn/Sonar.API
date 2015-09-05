1f419b91c68916b144dd92eea19031cee5858243
Karn Saheb <saheb.karn@live.com>
2015-09-05 09:38:33 -0400

    Updated responses to only return status code where possible.

--- Sonar.API/data/userSchema.js
+++ Sonar.API/data/userSchema.js
@@ -5,15 +5,13 @@ var mongoose = require('mongoose'); // import the model
 var userSchema = new mongoose.Schema({
 	id: { type: String, required: true },
 	name: { type: String, required: true },
-	description: { type: String, required: true }, 
+	description: { type: String }, 
 	location: { type: String, required: true },
-	following_count: { type: Number, required: true, "default": 0 },
 	following: { type: Array, required: true, "default": [] },
-	followers_count: { type: Number, required: true, "default": 0 },
 	followers: { type: Array, required: true, "default": [] },
-	tracks: { type: Number, required: true, "default": 0 },
-	starred_artists: { type: Number, required: true, "default": 0 },
-	liked_tracks: { type: Number, required: true, "default": 0 }
+	track_count: { type: Number, required: true, "default": 0 },
+	starred_artists: { type: Array, required: true, "default": [] },
+	liked_tracks: { type: Array, required: true, "default": [] }
 });
 
 // compile schema into a model
--- Sonar.API/routes.js
+++ Sonar.API/routes.js
@@ -2,334 +2,297 @@
 var Track = require('./data/trackSchema'); // import the model
 
 module.exports = function (app) {
-	
-	app.all('/api/*', function (req, res, next) {
-		res.contentType('json');
-		next();
-	});
-	
-	app.use(function (req, res) {
-		res.status(404);
-		res.json({ meta: { status: 400, message: 'Bad Request' }, response: {} });
-	});
-	
-	app.get('/api/users', function (req, res) {
-		
-		var conditions = {}; // we can use this object to supply search params ....
-		User.find(conditions, function (err, users) {
-			
-			console.log(users.length);
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-			}
-			
-			res.send({ meta: { status: 200, message: 'OK' }, response: { artist: users } });
-		});
-	});
-		
-	app.post('/api/users', function (req, res) {
-		var _user = new User();
-		_user.id = req.body.id;
-		_user.name = req.body.name;
-		_user.description = req.body.description;
-		_user.location = req.body.location;
-		
-		User.create(_user, function (err) {
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: {} });
-			}
-			
-			res.json({ meta: { status: 201, message: 'Created' }, response: { id: _user.id } });
-		});
-	});
-	
-	app.get('/api/users/:id', function (req, res, next) {
-		
-		var conditions = { id: req.params.id };
-		
-		User.find(conditions, function (err, user) {
-			
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-			}
-			
-			console.log(user);
-			if (user != null && user != []) {
-				res.send({ meta: { status: 200, message: 'OK' }, response: { artist: user } });
-			} else {
-				res.send({ meta: { status: 404, message: 'Not found' }, response: { artist: [] } });
-			}
-		});
+    
+    app.all('/api/*', function (req, res, next) {
+        res.contentType('json');
+        next();
+    });
+    
+    app.use(function (req, res) {
+        res.status(404);
+    });
+    
+    app.get('/api/users', function (req, res) {
+        
+        var conditions = {}; // we can use this object to supply search params ....
+        User.find(conditions, function (err, users) {
+            
+            console.log(users.length);
+            if (err) {
+                console.log(err);
+                res.status(500)
+            }
+            
+            res.send(users)
+        });
+    });
+    
+    app.post('/api/users', function (req, res) {
+        var _user = new User();
+        _user.id = req.body.id;
+        _user.name = req.body.name;
+        _user.description = req.body.description;
+        _user.location = req.body.location;
+        
+        User.create(_user, function (err) {
+            if (err) {
+                console.log(err);
+                res.satus(500);
+            }
+            
+            res.status(201);
+        });
+    });
+    
+    app.get('/api/users/:id', function (req, res, next) {
+        
+        var conditions = { id: req.params.id };
+        
+        User.find(conditions, function (err, user) {
+            
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            console.log(user);
+            if (user != null && user != []) {
+                res.send(user);
+            } else {
+                res.status(404);
+            }
+        });
 
-	});
-	
-	app.put('/api/users/:id/follow/:other_id', function (req, res) {
-		
-		var conditions = { id: req.params.id };
-		
-		User.find(conditions).exec(function (err, user) {
-			
-			if (user === null || user === undefined) {
-				res.send({ meta: { status: 404, message: 'Not found' }, response: {} })
-			} else {
-				var j = user.following.indexOf(req.params.other_id);
-				if (j != -1) {
-					user.following.push(user.id);
-					user.following_count = user.following_count + 1;
-					
-					User.find({ id: req.params.other_id }).exec(function (err, other_user) {
-						if (user === null || user === undefined) {
-							res.send({ meta: { status: 404, message: 'Not found' }, response: {} })
-						} else {
-							var i = other_user.followers.indexOf(user.id);
-							if (i != -1) {
-								other_user.followers.push(user.id);
-								other_user.followers_count = other_user.followers_count + 1;
-							}
-						}
-						
-						other_user.save(function (err) {
-							if (err) {
-								console.log(err);
-								res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-							}
-						});
-					});
-					
-					user.save(function (err) {
-						if (err) {
-							console.log(err);
-							res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-						}
-						
-						res.send({ meta: { status: 200, message: 'OK' }, response: {} });
-					});
-				}
-			}
-		});
-	});
-		
-	app.put('/api/users/:id/unfollow/:other_id', function (req, res) {
-		
-		var conditions = { id: req.params.id };
-		
-		User.find(conditions).exec(function (err, user) {
-			
-			if (user === null || user === undefined) {
-				res.send({ meta: { status: 404, message: 'Not found' }, response: {} })
-			} else {
-				var j = user.following.indexOf(req.params.other_id);
-				if (j != -1) {
-					user.following.splice(j, 1);
-					user.following_count = user.following_count - 1;
-
-					User.find({ id: req.params.other_id }).exec(function (err, other_user) {
-						if (user === null || user === undefined) {
-							res.send({ meta: { status: 404, message: 'Not found' }, response: {} })
-						} else {
-							var i = other_user.followers.indexOf(user.id);
-							if (i != -1) {
-								other_user.followers.splice(i, 1);
-								other_user.followers_count = other_user.followers_count - 1;
-							}
-						}
-
-						other_user.save(function (err) {
-							if (err) {
-								console.log(err);
-								res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-							}
-						});
-					});
-
-					user.save(function (err) {
-						if (err) {
-							console.log(err);
-							res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-						}
-						
-						res.send({ meta: { status: 200, message: 'OK' }, response: {} });
-					});
-				}
-			}
-		});
-	});
-	
-	app.get('/api/users/:id/following', function (req, res) {
-		
-		var conditions = { id: req.params.id };
-		
-		User.find(conditions, function (err, user) {
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-			} else {
-				var artist_list = [];
-				
-				for (index = 0; index < user.following.length; ++index) {
-					User.find({ id: user.following[index] }, function (error, user2) {
-						if (err) {
-							console.log(err);
-							res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-						}
-						
-						artist_list.push(user2);
-					});
-				}
-				
-				res.send({ meta: { status: 200, message: 'OK' }, response: { artists: artist_list } });
-			}
-		});
-	});
-	
-	app.get('/api/users/:id/followers', function (req, res) {
-		
-		var conditions = { id: req.params.id };
-		
-		User.find(conditions, function (err, user) {
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-			} else {
-				var artist_list = [];
-				
-				for (index = 0; index < user.followers.length; ++index) {
-					User.find({ id: user.followers[index] }, function (error, user2) {
-						if (err) {
-							console.log(err);
-							res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { artist: [] } });
-						}
-						
-						artist_list.push(user2);
-					});
-				}
-				
-				res.send({ meta: { status: 200, message: 'OK' }, response: { artists: artist_list } });
-			}
-		});
-	});
-	
-	app.get('/api/users/:id/tracks', function (req, res, next) {
-		
-		var conditions = { id: req.params.id };
-		
-		Track.find(conditions, function (err, tracks) {
-			
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { track: [] } });
-			}
-			
-			console.log(tracks);
-			if (tracks != null && tracks != []) {
-				res.send({ meta: { status: 200, message: 'OK' }, response: { track: tracks } });
-			} else {
-				res.send({ meta: { status: 404, message: 'Not found' }, response: { track: [] } });
-			}
-		});
-	});
-	
-	app.get('/api/tracks/:id', function (req, res, next) {
-		
-		//req.assert('id', 'Id is not a negative value').isLength(0, Number.MAX_VALUE);
-		var conditions = { id: req.params.id };
-		Track.find(conditions, function (err, tracks) {
-			
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { track: [] } });
-			}
-			
-			console.log(tracks);
-			if (tracks != null && tracks != []) {
-				res.send({ meta: { status: 200, message: 'OK' }, response: { track: tracks } });
-			} else {
-				res.send({ meta: { status: 404, message: 'Not found' }, response: { track: [] } });
-			}
-		});
-	});
-	
-	app.post('/api/tracks', function (req, res) {
-		
-		var _track = new Track();
-		_track.id = req.body.id;
-		_track.name = req.body.name;
-		_track.description = req.body.description;
-		_track.source = 'j';
-		
-		Track.create(_track, function (err) {
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: {} });
-			}
-			
-			res.json({ meta: { status: 201, message: 'Created' }, response: { id: _track.id } });
-		});
-	});
-	
-	app.get('/api/feed/new/city/:city', function (req, res, next) {
-		var conditions = { city: req.params.city }; // we can use this object to supply search params ....
-		Track.find(conditions, function (err, tracks) {
-			
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { track: [] } });
-			}
-			
-			console.log(tracks);
-			res.send({ meta: { status: 200, message: 'OK' }, response: { track: sortByKey(tracks, "uploaded") } });
-		});
-	});
-	
-	app.get('/api/feed/new/state/:state', function (req, res, next) {
-		var conditions = { state: req.params.state }; // we can use this object to supply search params ....
-		Track.find(conditions, function (err, tracks) {
-			
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { track: [] } });
-			}
-			
-			console.log(tracks);
-			res.send({ meta: { status: 200, message: 'OK' }, response: { track: sortByKey(tracks, "uploaded") } });
-		});
-	});
-		
-	app.get('/api/feed/hot/city/:city', function (req, res, next) {
-		var conditions = { city: req.params.city }; // we can use this object to supply search params ....
-		Track.find(conditions, function (err, tracks) {
-			
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { track: [] } });
-			}
-			
-			console.log(tracks);
-			res.send({ meta: { status: 200, message: 'OK' }, response: { track: sortByKey(tracks, "likes") } });
-		});
-	});
-	
-	app.get('/api/feed/hot/state/:state', function (req, res, next) {
-		var conditions = { state: req.params.state }; // we can use this object to supply search params ....
-		Track.find(conditions, function (err, tracks) {
-			
-			if (err) {
-				console.log(err);
-				res.send({ meta: { status: 500, message: 'Internal Server Error' }, response: { track: [] } });
-			}
-			
-			console.log(tracks);
-			res.send({ meta: { status: 200, message: 'OK' }, response: { track: sortByKey(tracks, "likes") } });
-		});
-	});
-	
-	function sortByKey(array, key) {
-		return array.sort(function (a, b) {
-			var x = a[key]; var y = b[key];
-			return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-		});
-	}
+    });
+    
+    app.put('/api/users/:id/follow/:other_id', function (req, res) {
+        
+        var conditions = { id: req.params.id };
+        
+        User.find(conditions).exec(function (err, user) {
+            
+            if (user === null || user === undefined) {
+                res.status(404);
+            } else {
+                var j = user.following.indexOf(req.params.other_id);
+                if (j != -1) {
+                    user.following.push(user.id);
+                    
+                    User.find({ id: req.params.other_id }).exec(function (err, other_user) {
+                        if (user === null || user === undefined) {
+                            res.status(404);
+                        } else {
+                            var i = other_user.followers.indexOf(user.id);
+                            if (i != -1) {
+                                other_user.followers.push(user.id);
+                            }
+                        }
+                        
+                        other_user.save(function (err) {
+                            if (err) {
+                                console.log(err);
+                                res.status(500);
+                            }
+                        });
+                    });
+                    
+                    user.save(function (err) {
+                        if (err) {
+                            console.log(err);
+                            res.status(500);
+                        }
+                        
+                        res.status(200);
+                    });
+                }
+            }
+        });
+    });
+    
+    app.put('/api/users/:id/unfollow/:other_id', function (req, res) {
+        
+        var conditions = { id: req.params.id };
+        
+        User.find(conditions).exec(function (err, user) {
+            
+            if (user === null || user === undefined) {
+                res.status(404);
+            } else {
+                var j = user.following.indexOf(req.params.other_id);
+                if (j != -1) {
+                    user.following.splice(j, 1);
+                    
+                    User.find({ id: req.params.other_id }).exec(function (err, other_user) {
+                        if (user === null || user === undefined) {
+                            res.status(404);
+                        } else {
+                            var i = other_user.followers.indexOf(user.id);
+                            if (i != -1) {
+                                other_user.followers.splice(i, 1);
+                            }
+                        }
+                        
+                        other_user.save(function (err) {
+                            if (err) {
+                                console.log(err);
+                                res.status(500);
+                            }
+                        });
+                    });
+                    
+                    user.save(function (err) {
+                        if (err) {
+                            console.log(err);
+                            res.status(500);
+                        }
+                        
+                        res.status(200);
+                    });
+                }
+            }
+        });
+    });
+    
+    app.get('/api/users/:id/following', function (req, res) {
+        
+        var conditions = { id: req.params.id };
+        
+        User.find(conditions, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+            } else {
+                res.send(user.following);
+            }
+        });
+    });
+    
+    app.get('/api/users/:id/followers', function (req, res) {
+        
+        var conditions = { id: req.params.id };
+        
+        User.find(conditions, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+            } else {
+                res.send(user.followers);
+            }
+        });
+    });
+    
+    app.get('/api/users/:id/tracks', function (req, res, next) {
+        
+        var conditions = { id: req.params.id };
+        
+        Track.find(conditions, function (err, tracks) {
+            
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            console.log(tracks);
+            if (tracks != null && tracks != []) {
+                res.send(tracks);
+            } else {
+                res.status(404);
+            }
+        });
+    });
+    
+    app.post('/api/tracks', function (req, res) {
+        
+        var _track = new Track();
+        _track.id = req.body.id;
+        _track.name = req.body.name;
+        _track.description = req.body.description;
+        _track.source = 'j';
+        
+        Track.create(_track, function (err) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            res.status(201);
+        });
+    });
+    
+    app.get('/api/tracks/:id', function (req, res, next) {
+        
+        var conditions = { id: req.params.id };
+        Track.find(conditions, function (err, tracks) {
+            
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            if (tracks != null && tracks != []) {
+                res.send(tracks);
+            } else {
+                res.status(404);
+            }
+        });
+    });
+    
+    app.get('/api/feed/new/city/:city', function (req, res, next) {
+        var conditions = { city: req.params.city }; // we can use this object to supply search params ....
+        Track.find(conditions, function (err, tracks) {
+            
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            res.send(sortByKey(tracks, "uploaded"));
+        });
+    });
+    
+    app.get('/api/feed/new/state/:state', function (req, res, next) {
+        var conditions = { state: req.params.state }; // we can use this object to supply search params ....
+        Track.find(conditions, function (err, tracks) {
+            
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            res.send(sortByKey(tracks, "uploaded"));
+        });
+    });
+    
+    app.get('/api/feed/hot/city/:city', function (req, res, next) {
+        var conditions = { city: req.params.city }; // we can use this object to supply search params ....
+        Track.find(conditions, function (err, tracks) {
+            
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            res.send(sortByKey(tracks, "likes"));
+        });
+    });
+    
+    app.get('/api/feed/hot/state/:state', function (req, res, next) {
+        var conditions = { state: req.params.state }; // we can use this object to supply search params ....
+        Track.find(conditions, function (err, tracks) {
+            
+            if (err) {
+                console.log(err);
+                res.status(500);
+            }
+            
+            res.send(sortByKey(tracks, "likes"));
+        });
+    });
+    
+    function sortByKey(array, key) {
+        return array.sort(function (a, b) {
+            var x = a[key]; var y = b[key];
+            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        });
+    }
 };
